8b1994061450d414b660794073775dd7ddc7626c
Tim Garvin <tcgarvin@gmail.com>
2021-12-09 01:03:32 -0500

    Day 9 slightly cleaned

--- 09/basin.py
+++ 09/basin.py
@@ -9,32 +9,32 @@ def get_puzzle_input():
             puzzle_input.append([int(c) for c in line.strip()])
     return puzzle_input
 
-def solve_part_1(puzzle_input):
-    #print(puzzle_input)
+def solve_part_1(heightmap):
+    #print(heightmap)
     risk_level = 0
-    for i in range(len(puzzle_input)):
-        row = puzzle_input[i]
-        for j in range(len(puzzle_input[0])):
-            level = puzzle_input[i][j]
-            if i > 0 and puzzle_input[i-1][j] <= level:
+    for i in range(len(heightmap)):
+        row = heightmap[i]
+        for j in range(len(heightmap[0])):
+            height = heightmap[i][j]
+            if i > 0 and heightmap[i-1][j] <= height:
                 continue
-            if i < len(puzzle_input)-1 and puzzle_input[i+1][j] <= level:
+            if i < len(heightmap)-1 and heightmap[i+1][j] <= height:
                 continue
-            if j > 0 and puzzle_input[i][j-1] <= level:
+            if j > 0 and heightmap[i][j-1] <= height:
                 continue
-            if j < len(row)-1 and row[j+1] <= level:
+            if j < len(row)-1 and row[j+1] <= height:
                 continue
 
-            #print(i,j,level)
-            risk_level += level + 1
+            #print(i,j,height)
+            risk_level += height + 1
 
     return risk_level
 
-def solve_part_2(puzzle_input):
+def solve_part_2(heightmap):
     basin_sizes = []
-    basin_membership = {}
-    for i in range(len(puzzle_input)):
-        for j in range(len(puzzle_input[0])):
+    basin_membership = set()
+    for i in range(len(heightmap)):
+        for j in range(len(heightmap[0])):
             #print("-", i,j)
             candidates = [(i,j)]
             basin_size = 0
@@ -42,14 +42,14 @@ def solve_part_2(puzzle_input):
                 candidate = candidates.pop()
                 ci, cj = candidate
                 #print(candidate)
-                if ci < 0 or ci >= len(puzzle_input):
+                if ci < 0 or ci >= len(heightmap):
                     #print("ibounds")
                     continue
-                if cj < 0 or cj >= len(puzzle_input[0]):
+                if cj < 0 or cj >= len(heightmap[0]):
                     #print("jbounds")
                     continue
-                if puzzle_input[ci][cj] >= 9:
-                    #print("level")
+                if heightmap[ci][cj] >= 9:
+                    #print("height")
                     continue
                 if candidate in basin_membership:
                     #print("spoken for")
@@ -57,7 +57,7 @@ def solve_part_2(puzzle_input):
 
                 #print("check")
                 basin_size += 1
-                basin_membership[candidate] = True
+                basin_membership.add(candidate)
                 candidates.append((ci+1,cj))
                 candidates.append((ci-1,cj))
                 candidates.append((ci,cj+1))
@@ -68,7 +68,7 @@ def solve_part_2(puzzle_input):
 
     #print(basin_sizes)
     largest = list(nlargest(3, basin_sizes))
-    print(largest)
+    #print(largest)
 
     return largest[0] * largest[1] * largest[2]
 
