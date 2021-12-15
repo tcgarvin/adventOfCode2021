aa9659ce04afbf88829c12b50a87fc83e4e45902
Tim Garvin <tcgarvin@gmail.com>
2021-12-15 01:05:26 -0500

    Cleaner day 15

--- 15/chiton.py
+++ 15/chiton.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from heapq import heappush, heappop, heappushpop
+from heapq import heappush, heappop
 from rich import print
+
 
 def get_puzzle_input():
     puzzle_input = {}
@@ -10,30 +11,33 @@ def get_puzzle_input():
                 puzzle_input[(i,j)] = int(c)
     return puzzle_input
 
-def solve_part_1(puzzle_input):
+
+def find_cheapest_path(cavern):
     heap = [(0,(0,0))]
     location_costs = defaultdict(lambda: float('inf'))
     while len(heap) > 0:
         current_cost, coords = heappop(heap)
         i,j = coords
         for neighbor in ((i,j+1), (i, j-1), (i+1, j), (i-1, j)):
-            if neighbor not in puzzle_input:
+            if neighbor not in cavern:
                 continue
 
-            neighbor_cost = current_cost + puzzle_input[neighbor]
+            neighbor_cost = current_cost + cavern[neighbor]
             if neighbor_cost < location_costs[neighbor]:
                 location_costs[neighbor] = neighbor_cost
                 heappush(heap, (neighbor_cost, neighbor))
 
     return location_costs[max(location_costs.keys())]
+
+
+def solve_part_1(puzzle_input):
+    return find_cheapest_path(puzzle_input)
             
 
 def solve_part_2(puzzle_input):
     puzzle_input_width, puzzle_input_height = max(puzzle_input.keys())
     puzzle_input_height += 1
     puzzle_input_width += 1
-
-    print(puzzle_input_width, puzzle_input_height)
 
     whole_map = puzzle_input.copy()
     for dj in range(5):
@@ -48,23 +52,8 @@ def solve_part_2(puzzle_input):
                     new_cost -= 9
                 whole_map[(i+(di*puzzle_input_width), j+(dj*puzzle_input_height))] = new_cost
 
-    print(max(whole_map.keys()))
+    return find_cheapest_path(whole_map)
 
-    heap = [(0,(0,0))]
-    location_costs = defaultdict(lambda: float('inf'))
-    while len(heap) > 0:
-        current_cost, coords = heappop(heap)
-        i,j = coords
-        for neighbor in ((i,j+1), (i, j-1), (i+1, j), (i-1, j)):
-            if neighbor not in whole_map:
-                continue
-
-            neighbor_cost = current_cost + whole_map[neighbor]
-            if neighbor_cost < location_costs[neighbor]:
-                location_costs[neighbor] = neighbor_cost
-                heappush(heap, (neighbor_cost, neighbor))
-
-    return location_costs[max(location_costs.keys())]
 
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input()
