ebbadae26c9e5fd35b421502c39bf7dcffacd74a
Tim Garvin <tcgarvin@gmail.com>
2021-12-22 21:59:55 -0500

    Day 22 Part 2

--- 22/cubes.py
+++ 22/cubes.py
@@ -2,40 +2,136 @@ from collections import defaultdict
 import re
 from rich import print
 
+
 def get_puzzle_input():
     commands = []
     with open("input.txt") as input_txt:
         for line in input_txt:
-            match = re.search("x=(-?[0-9]+)..(-?[0-9]+),y=(-?[0-9]+)..(-?[0-9]+),z=(-?[0-9]+)..(-?[0-9]+)", line)
+            reg = re.compile("""
+                x=(-?[0-9]+)..(-?[0-9]+),
+                y=(-?[0-9]+)..(-?[0-9]+),
+                z=(-?[0-9]+)..(-?[0-9]+)
+            """, re.X)
+            match = reg.search(line)
             on = line.startswith("on")
-            xrange = sorted((int(match.group(1)), int(match.group(2))))
-            yrange = sorted((int(match.group(3)), int(match.group(4))))
-            zrange = sorted((int(match.group(5)), int(match.group(6))))
+            # Note ranges become of form [x,y) exclusive instead of [x,y] inclusive
+            xrange = sorted((int(match.group(1)), int(match.group(2))+1))
+            yrange = sorted((int(match.group(3)), int(match.group(4))+1))
+            zrange = sorted((int(match.group(5)), int(match.group(6))+1))
             commands.append((on, xrange, yrange, zrange))
     return commands
 
+
+class BinarySpacePartition:
+    def __init__(self, on, xrange, yrange, zrange):
+        self.on = on
+        self.xrange = xrange
+        self.yrange = yrange
+        self.zrange = zrange
+        self._left = None
+        self._right = None
+
+
+    def engulfed_by(self, xrange, yrange, zrange):
+        result = True
+        result &= self.xrange[0] >= xrange[0]
+        result &= self.xrange[1] <= xrange[1]
+        result &= self.yrange[0] >= yrange[0]
+        result &= self.yrange[1] <= yrange[1]
+        result &= self.zrange[0] >= zrange[0]
+        result &= self.zrange[1] <= zrange[1]
+        return result
+
+
+    def volume(self):
+        return ((self.xrange[1] - self.xrange[0]) * 
+                (self.yrange[1] - self.yrange[0]) * 
+                (self.zrange[1] - self.zrange[0]))
+
+
+    def intersects(self, xrange, yrange, zrange):
+        # Seems like intersection is true unless we can identify a gap in a
+        # particular axis?
+        intersects = True
+        intersects &= self.xrange[0] < xrange[1]
+        intersects &= self.xrange[1] > xrange[0]
+        intersects &= self.yrange[0] < yrange[1]
+        intersects &= self.yrange[1] > yrange[0]
+        intersects &= self.zrange[0] < zrange[1]
+        intersects &= self.zrange[1] > zrange[0]
+        return intersects
+
+
+    def count_on(self):
+        #print(self.xrange, self.yrange, self.zrange, self.volume())
+        if self.has_children():
+            return self._left.count_on() + self._right.count_on()
+
+        return self.volume() * self.on
+
+
+    def has_children(self):
+        return self._left is not None and self._right is not None
+
+
+    def apply_command(self, on, xrange, yrange, zrange):
+        if not self.intersects(xrange, yrange, zrange):
+            return
+
+        elif self.engulfed_by(xrange, yrange, zrange):
+            self.on = on
+            self._left = None
+            self._right = None
+            return
+
+        elif self.has_children():
+            pass # Call out to children below
+
+        # This does not seem great
+        elif self.xrange[0] < xrange[0]:
+            self._left = BinarySpacePartition(self.on, (self.xrange[0], xrange[0]), self.yrange, self.zrange)
+            self._right = BinarySpacePartition(self.on, (xrange[0], self.xrange[1]), self.yrange, self.zrange)
+
+        elif self.xrange[1] > xrange[1]:
+            self._left = BinarySpacePartition(self.on, (self.xrange[0], xrange[1]), self.yrange, self.zrange)
+            self._right = BinarySpacePartition(self.on, (xrange[1], self.xrange[1]), self.yrange, self.zrange)
+
+        elif self.yrange[0] < yrange[0]:
+            self._left = BinarySpacePartition(self.on, self.xrange, (self.yrange[0], yrange[0]), self.zrange)
+            self._right = BinarySpacePartition(self.on, self.xrange, (yrange[0], self.yrange[1]), self.zrange)
+
+        elif self.yrange[1] > yrange[1]:
+            self._left = BinarySpacePartition(self.on, self.xrange, (self.yrange[0], yrange[1]), self.zrange)
+            self._right = BinarySpacePartition(self.on, self.xrange, (yrange[1], self.yrange[1]), self.zrange)
+
+        elif self.zrange[0] < zrange[0]:
+            self._left = BinarySpacePartition(self.on, self.xrange, self.yrange, (self.zrange[0], zrange[0]))
+            self._right = BinarySpacePartition(self.on, self.xrange, self.yrange, (zrange[0], self.zrange[1]))
+
+        elif self.zrange[1] > zrange[1]:
+            self._left = BinarySpacePartition(self.on, self.xrange, self.yrange, (self.zrange[0], zrange[1]))
+            self._right = BinarySpacePartition(self.on, self.xrange, self.yrange, (zrange[1], self.zrange[1]))
+
+        self._left.apply_command(on, xrange, yrange, zrange)
+        self._right.apply_command(on, xrange, yrange, zrange)
+
+
 def solve_part_1(commands):
-    cubes = defaultdict(bool)
-    i = 0
+    cubes = BinarySpacePartition(0, (-50, 51), (-50, 51), (-50, 51))
     for on, xrange, yrange, zrange in commands:
-        print (i)
+        cubes.apply_command(on, xrange, yrange, zrange)
 
-        for x in range(xrange[0], xrange[1]+1):
-            if x > 50 or x < -50:
-                continue
-            for y in range(yrange[0], yrange[1]+1):
-                if y > 50 or y < -50:
-                    continue
-                for z in range(zrange[0], zrange[1]+1):
-                    if z > 50 or z < -50:
-                        continue
-                    cubes[(x,y,z)] = on
+    return cubes.count_on()
 
-        i += 1
-    return sum(cubes.values())
 
-def solve_part_2(puzzle_input):
-    return ""
+def solve_part_2(commands):
+    box_bounds = (-200000, 200000)
+    cubes = BinarySpacePartition(0, box_bounds, box_bounds, box_bounds)
+    for on, xrange, yrange, zrange in commands:
+        cubes.apply_command(on, xrange, yrange, zrange)
+
+    return cubes.count_on()
+
 
 if __name__ == "__main__":
     puzzle_input = get_puzzle_input()
